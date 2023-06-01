0cc1535077ba397ca2e60a930d4d4d529835e809
Alex <alex-admin@Ubuntu.myguest.virtualbox.org>
2023-06-01 04:02:46 -0400

    Tip Calculator express app created

--- Week 9 HW/Week 9 Practical/server.js
+++ Week 9 HW/Week 9 Practical/server.js
@@ -6,10 +6,21 @@ app.listen(port, () => {
     console.log('Server is running')
 })
 
-
-app.get('/greeting', function (req, res) {
-    res.send('Hello')
-})
+//Greeting
 app.get('/greeting/:name', function (req, res) {
     res.send(`Hello ${req.params.name}`)
 })
+
+//Tip
+app.get('/tip/:total/:tipPercentage', function (req, res) {
+    res.send(`Your tip will be ${req.params.total*(req.params.tipPercentage*.01)} dollaridoos mate`)
+})
+
+//Magic 8 Ball
+app.get('/magic/:phrase', function (req, res) {
+    res.send(
+        
+    )
+})
+
+//Fibonacci
