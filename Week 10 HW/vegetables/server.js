f87828f62c8d0114e15fd025e4dffb6508c034ab
Alex <alex-admin@Ubuntu.myguest.virtualbox.org>
2023-06-03 06:13:35 -0400

    vegetables v2

--- Week 10 HW/vegetables/server.js
+++ Week 10 HW/vegetables/server.js
@@ -11,6 +11,9 @@ const app = express()
 //router.express.json is used when building an api
 app.use(express.urlencoded({extended: true}))
 
+app.set('view engine', 'jsx')
+app.engine('jsx', jsxEngine())
+
 //inject data from Mongoose via MONGO_URI
 mongoose.connect(process.env.MONGO_URI)
 mongoose.connection.once('open', () =>{
@@ -25,6 +28,10 @@ app.listen (PORT, () => {
 //INDEX - table of contents. List all of the fruits
 
 //NEW - show user form to fill out to create a fruit
+//res.render does not need the forward slash before fruitsspecifically 
+app.get('/vegetables/new', (req, res) => {
+    res.render('vegetables/New')
+})
 
 //DELETE - backend only functionality that is used to delete a fruit
 
@@ -32,10 +39,16 @@ app.listen (PORT, () => {
 
 //CREATE - backend only functionality that is used to create a fruit
 app.post('/vegetables', async (req, res) => {
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true
+    }else {
+        req.body.readyToEat = false
+    }
     try{
-
+        const createdVegetable = await Vegetable.create(req.body)
+        res.send(createdVegetable)
     }catch(error) {
-        
+        res.status(400).send({message: error.message})
     }
 })
 //EDIT - show user a form to edit the fruit
